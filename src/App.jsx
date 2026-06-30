886774cb1dc5136069eb7c2ae654e0a3bfd68887
Kate465 <kategokeborate@gmail.com>
2026-06-30 20:46:28 +0100

    added a footer and Github link

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-
-
-
-
 function App() {
-   return<div className="App">Weather App</div>;
+   return<div className="App">Weather App
+   <footer> This project was coded by Kate and is < a href= "https://github.com/Kate465/reactproject" target="_blank"> open sourced on Github
+    </a>
+   </footer>
+   </div>;
     }
 
 export default App;
