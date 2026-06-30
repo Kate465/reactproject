327d8756d5bee9a4b9e4416959c48f35d2ebab73
Kate465 <kategokeborate@gmail.com>
2026-06-30 21:05:00 +0100

    added H1 to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 function App() {
-   return<div className="App">Weather App
+   return<div className="App"> 
+   <h1>Weather App</h1>
    <footer> This project was coded by Kate and is < a href= "https://github.com/Kate465/reactproject" target="_blank"> open sourced on Github
     </a>
    </footer>
