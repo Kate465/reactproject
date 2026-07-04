3c1e1487d003bc7ab677f8c4d76762ec0a5c19c3
Kate465 <kategokeborate@gmail.com>
2026-07-04 19:37:50 +0100

    added weather component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import Weather from './Weather';
 
 function App() {
    return<div className="App"> 
    <div className="container">
    <h1>Weather App</h1>
+   < Weather/>
    <footer> This project was coded by Kate and is < a href= "https://github.com/Kate465/reactproject" target="_blank"> open sourced on Github
     </a>
    </footer>
    </div>;
-   </div>
+   </div>;
     }
 
 export default App;
