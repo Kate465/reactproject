72b5cf605aba64db2cc2d2f9043bffd7de971a23
Kate465 <kategokeborate@gmail.com>
2026-07-04 19:11:11 +0100

    added bootstrap to project

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 function App() {
    return<div className="App"> 
+   <div className="container">
    <h1>Weather App</h1>
    <footer> This project was coded by Kate and is < a href= "https://github.com/Kate465/reactproject" target="_blank"> open sourced on Github
     </a>
    </footer>
    </div>;
+   </div>
     }
 
 export default App;
